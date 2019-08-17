6fa0a7ccc38d8edb43e20d6e38e47fa40c630c13
michaelrhodes <spam@michaelrhod.es>
2019-08-17 13:59:59 +1000

    Fix bit-shifting bug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = (s, b) => (b = new Uint8Array((s % 8 ? 1 : 0) + s >> 3), {
+module.exports = (s, b) => (b = new Uint8Array((s % 8 ? 1 : 0) + (s >> 3)), {
   buffer: b,
   get: (i, v) => !!(b[i >> 3] & (128 >> (i % 8))),
   set: (i, v) => !!(v ? b[i >> 3] |= 128 >> (i % 8) : b[i >> 3] &= ~(128 >> i % 8))
